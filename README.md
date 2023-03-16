3f4fa8ecbbb68c16f5a1da35fe98f09a005d220b
Miguelin69 <miguel2234505@correo.uis.edu.co>
2023-03-15 20:34:53 -0500

    se agrego programa

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Juego_dados
-s
+## Juego_dados 
+
+Programa para saber la cara opuesta de un dado
+
+# Analisis
+
+# Diseño
+![Diagrama de flujo](diagramaXX.png "Diagrama de flujo")
---
+++ juego_dados.py
@@ -0,0 +1,17 @@
+print("Pon el número de de la cara del dado: ")
+a = int(input())
+
+if a == 1:
+    print("  su opuesto es 6  ")
+elif a == 2:
+    print("  su opuesto es 5  ")
+elif a == 3:
+    print("  su opuesto es 4  ")
+elif a == 4:
+    print("  su opuesto es 3  ")
+elif a == 5:
+    print("  su opuesto es 2  ")
+elif a == 6:
+    print("  su opuesto es 1  ")
+else:
+    print(" NO COLOCASTE EL NUMERO DE LA CARA BIEN ")
